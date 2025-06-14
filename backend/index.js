d05f418230f53b8fb2f475c83030cd85a0c98e90
dinesh-gurusamy <dineshgurusamy18@gmail.com>
2025-06-15 00:04:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: 'https://academia-1-c0bl.onrender.com',
   credentials: true,
 }));
 app.use(express.json());
